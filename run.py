6e2f2cfe4e1d19f5999c08b683cd8e0474b8a760
RenzoFelix <140411998+renzo-felix@users.noreply.github.com>
2026-05-08 16:46:37 -0500

    cambios par aq funcione

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("back.api:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("back.api.server:app", host="0.0.0.0", port=8000, reload=False)
